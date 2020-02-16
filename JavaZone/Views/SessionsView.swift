fe6c94b9e386f1379eb80e8af05556130864aa40
Chris Searle <chris@chrissearle.org>
2020-02-16 20:28:14 +0100

    Refresh on session view if no sessions present

--- JavaZone/Views/SessionsView.swift
+++ JavaZone/Views/SessionsView.swift
@@ -5,5 +5,10 @@ struct SessionsView: View {
     
     var body: some View {
         SessionsListView(sessions: sessions, title: "Sessions")
+            .onAppear {
+                if (self.sessions.count == 0) {
+                    SessionService.refresh()
+                }
+        }
     }
 }
