eb508cc6cfca0ec9f902a1b32a469439b3283e7a
Chris Searle <chris@chrissearle.org>
2020-02-25 12:17:32 +0100

    Refs #6. Generate a QR code from the scanned QR code.
    Not sure if we need this but nice to know that we can do so if we do need to.
    
    Note that it will not be identical to the original - but it will still be a valid QR code for the scanned data.

--- JavaZone/Extensions/String+Utils.swift
+++ JavaZone/Extensions/String+Utils.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 
 extension String {
     func contains(_ candidate: String) -> Bool {
@@ -8,6 +9,38 @@ extension String {
     func deletePrefix(_ prefix: String) -> String {
         guard self.hasPrefix(prefix) else {return self}
         return String(self.dropFirst(prefix.count))
+    }
+    
+    func generateQRCode() -> UIImage? {
+        var uiImage: UIImage?
+
+        if let data = self.data(using: String.Encoding.ascii),
+            let filter = CIFilter(name: "CIQRCodeGenerator",
+                                 parameters: ["inputMessage": data,
+                                              "inputCorrectionLevel": "H"]) {
+
+            if let outputImage = filter.outputImage,
+                let cgImage = CIContext().createCGImage(outputImage,
+                                                        from: outputImage.extent) {
+                
+                let size = CGSize(width: outputImage.extent.width * 7.0,
+                                  height: outputImage.extent.height * 7.0)
+                
+                UIGraphicsBeginImageContext(size)
+
+                if let context = UIGraphicsGetCurrentContext() {
+                    context.interpolationQuality = .none
+                    context.draw(cgImage,
+                                 in: CGRect(origin: .zero,
+                                            size: size))
+                    uiImage = UIGraphicsGetImageFromCurrentImageContext()
+                }
+
+                UIGraphicsEndImageContext()
+            }
+        }
+
+        return uiImage
     }
 }
 
