5a8cb2036d28118c3ce7e0d549e89174ea89efb6
Chris Searle <chris@chrissearle.org>
2020-02-25 21:51:48 +0100

    Refs #7 - testing a grid component

--- JavaZone/Views/Partner/PartnerListView.swift
+++ JavaZone/Views/Partner/PartnerListView.swift
@@ -1,9 +1,18 @@
 import SwiftUI
+import WaterfallGrid
 
 struct PartnerListView: View {
     var body: some View {
         VStack {
             Text("Partner List")
+            WaterfallGrid((0..<20), id: \.self) { index in
+                Image(systemName: "\(index).circle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.black)
+            }
+            .gridStyle(columns: 3, spacing: 10)
+            .padding()
             Spacer()
         }
     }
