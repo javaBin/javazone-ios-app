1efcd6900122d564cff5e0f65ab343c882d99115
Chris Searle <chris@chrissearle.org>
2020-03-02 17:49:10 +0100

    Refs #7 - Start work on partner scan

--- JavaZone/Views/Partner/PartnerListView.swift
+++ JavaZone/Views/Partner/PartnerListView.swift
@@ -1,18 +1,20 @@
 import SwiftUI
 import WaterfallGrid
+import os
 
 struct PartnerListView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     @FetchRequest(fetchRequest: Partner.getPartners()) var partners: FetchedResults<Partner>
 
     @State private var isShowingRefreshAlert = false
-    @State private var refreshAlertTitle = ""
-    @State private var refreshAlertMessage = ""
     @State private var refreshFatal = false
     @State private var refreshFatalMessage = ""
     
-    @State private var scannedData = ""
     @State private var showingScanSheet = false
+    
+    @State private var isShowingAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
 
     private var idiom : UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
     private var isPortrait : Bool { UIDevice.current.orientation.isPortrait }
@@ -34,6 +36,14 @@ struct PartnerListView: View {
         }
     }
     
+    let simulatorData = """
+{
+  "name": "Test Partner 7",
+  "url": "https://java.no/7",
+  "logo": "https://www.java.no/javazone-ios-app/testpartner.png"
+}
+"""
+    
     var body: some View {
         VStack {
             HStack {
@@ -49,11 +59,10 @@ struct PartnerListView: View {
                 }.onTapGesture {
                     self.showingScanSheet = true
                 }.sheet(isPresented: $showingScanSheet) {
-                    ScannerView(data: Binding(
-                        get: { self.scannedData },
+                    ScannerView(simulatorData: self.simulatorData, data: Binding(
+                        get: { "" },
                         set: { (newVal) in
-                            self.scannedData = newVal
-                            // Here we can look and update the matching partner
+                            self.partnerScan(value: newVal)
                         }
                     ))
                 }
@@ -68,21 +77,27 @@ struct PartnerListView: View {
                 self.refreshPartners(force: false)
             })
             .alert(isPresented: $isShowingRefreshAlert) {
-                Alert(title: Text(self.refreshAlertTitle),
-                      message: Text(self.refreshAlertMessage),
+                Alert(title: Text(self.alertTitle),
+                      message: Text(self.alertMessage),
                       dismissButton: Alert.Button.default(
                         Text("OK"), action: {
                             if (self.refreshFatal) {
                                 fatalError(self.refreshFatalMessage)
                             }
                             
-                            self.refreshAlertMessage = ""
-                            self.refreshAlertTitle = ""
+                            self.alertMessage = ""
+                            self.alertTitle = ""
                             self.refreshFatalMessage = ""
                             self.refreshFatal = false
                       }
                     )
                 )
+            }
+            .alert(isPresented: $isShowingAlert) {
+                Alert(title: Text(self.alertTitle),
+                      message: Text(self.alertMessage),
+                      dismissButton: .default(Text("OK"))
+                    )
             }
             Spacer()
         }
@@ -94,20 +109,27 @@ struct PartnerListView: View {
             // If we fail to fetch but have partners _ this list changes so rarely that we ignore and continue.
             if (status == .Fail && self.partners.count == 0) {
                 self.refreshFatal = false
-                self.refreshAlertTitle = "Refresh failed"
-                self.refreshAlertMessage = message
+                self.alertTitle = "Refresh failed"
+                self.alertMessage = message
                 self.refreshFatalMessage = ""
                 self.isShowingRefreshAlert = true
             }
             
             if (status == .Fatal) {
                 self.refreshFatal = true
-                self.refreshAlertTitle = "Refresh failed"
-                self.refreshAlertMessage = message
+                self.alertTitle = "Refresh failed"
+                self.alertMessage = message
                 self.refreshFatalMessage = logMessage
                 self.isShowingRefreshAlert = true
             }
         }
+    }
+    
+    func partnerScan(value: String) {
+        os_log("Scanned badge", log: .ui, type: .debug)
+        
+        // TODO - parse the json
+        
     }
 }
 
