1f4864b1a80301d33da622196cb9eae9d3205879
Chris Searle <chris@chrissearle.org>
2021-08-18 10:51:33 +0200

    Fixes #31 - use progress view for autorefresh

--- JavaZone/Views/ContentView.swift
+++ JavaZone/Views/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
             }
             
             if (blockingRefresh) {
-                SpinnerView()
+                ProgressView("Refreshing sessions")
             }
         }
     }
