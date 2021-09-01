1825d8af0ca4ff86301ac7c07a2f0dee6e49f5bf
Chris Searle <chris@chrissearle.org>
2021-09-01 22:08:42 +0200

    For now - only show partners list

--- JavaZone/Views/Partner/PartnersView.swift
+++ JavaZone/Views/Partner/PartnersView.swift
@@ -1,11 +1,12 @@
 import SwiftUI
 
 struct PartnersView: View {
-    @State private var selectorIndex = 0
+    @State private var selectorIndex = 1
     
     var body: some View {
             VStack {
                 Text("Partners").font(.headline)
+                #if DEBUGX
                 Picker("", selection: $selectorIndex) {
                     Text("My Pass").tag(0)
                     Text("Partners").tag(1)
@@ -13,6 +14,7 @@ struct PartnersView: View {
                 }
                 .pickerStyle(SegmentedPickerStyle())
                 .padding(.horizontal)
+                #endif
 
                 if (selectorIndex == 0) {
                     PartnerBadgeView()
