623bb8043a453a3085c3053373e41215663a3457
Chris Searle <chris@chrissearle.org>
2021-09-10 15:34:54 +0200

    Latest fastlane

---
+++ fastlane/Fastfile.swift
@@ -0,0 +1,24 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+	func betaLane() {
+	desc("Push a new beta build to TestFlight")
+		ensureGitStatusClean()
+		incrementBuildNumber(xcodeproj: "JavaZone.xcodeproj")
+		commitVersionBump(xcodeproj: "JavaZone.xcodeproj")
+		addGitTag()
+		pushToGitRemote()
+		getProvisioningProfile(app_identifier: "net.chrissearle.incogito.JavaZone")
+		getProvisioningProfile(app_identifier: "net.chrissearle.incogito.JavaZone.Duke")
+		buildApp(scheme: "JavaZone")
+		uploadToTestflight()
+	}
+}
