c4a236941ad456d3e3e1423dfc885ea22d84cab3
Chris Searle <chris@chrissearle.org>
2021-09-10 16:36:31 +0200

    Revert to fastlane ruby syntax

--- fastlane/Appfile.swift
+++
@@ -1,7 +0,0 @@
-var appIdentifier: String { return "net.chrissearle.incogito.JavaZone" } // The bundle identifier of your app
-
-var itcTeamId: String? { return "118635576" } // App Store Connect Team ID
-var teamID: String { return "X4FF6CYH8M" } // Apple Developer Portal Team ID
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
