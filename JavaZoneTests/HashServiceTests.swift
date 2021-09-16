530ab1e8ac069ec23613cd2aab27ed245d17d3e4
Chris Searle <chris@chrissearle.org>
2021-09-16 17:19:15 +0200

    Remove outdated test

--- JavaZoneTests/HashServiceTests.swift
+++ JavaZoneTests/HashServiceTests.swift
@@ -8,10 +8,4 @@ class HashServiceTests: XCTestCase {
 
     override func tearDown() {
     }
-
-    func testHash() {
-        let hash = HashService.hash(salt: "12345", value: "JavaZone")
-        
-        XCTAssertEqual(hash, "67f9ae3824f6ba51dce9119c2897e542f63eaa4a332c019444c369c31f4b3c83a2e31b1346007a5401b2537e126dfb83383b355054be48a6cfb18f8a9bddbbab")
-    }
 }
