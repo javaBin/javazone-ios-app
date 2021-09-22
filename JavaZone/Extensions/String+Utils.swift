f68b78c91cb3d4ec93adb583edef2ec595343f8c
Chris Searle <chris@chrissearle.org>
2021-09-22 12:44:40 +0200

    Remove unused method

--- JavaZone/Extensions/String+Utils.swift
+++ JavaZone/Extensions/String+Utils.swift
@@ -10,39 +10,7 @@ extension String {
         guard self.hasPrefix(prefix) else {return self}
         return String(self.dropFirst(prefix.count))
     }
-    
-    func generateQRCode() -> UIImage? {
-        var uiImage: UIImage?
-
-        if let data = self.data(using: String.Encoding.ascii),
-            let filter = CIFilter(name: "CIQRCodeGenerator",
-                                 parameters: ["inputMessage": data,
-                                              "inputCorrectionLevel": "H"]) {
-
-            if let outputImage = filter.outputImage,
-                let cgImage = CIContext().createCGImage(outputImage,
-                                                        from: outputImage.extent) {
-                
-                let size = CGSize(width: outputImage.extent.width * 7.0,
-                                  height: outputImage.extent.height * 7.0)
-                
-                UIGraphicsBeginImageContext(size)
-
-                if let context = UIGraphicsGetCurrentContext() {
-                    context.interpolationQuality = .none
-                    context.draw(cgImage,
-                                 in: CGRect(origin: .zero,
-                                            size: size))
-                    uiImage = UIGraphicsGetImageFromCurrentImageContext()
-                }
-
-                UIGraphicsEndImageContext()
-            }
-        }
-
-        return uiImage
-    }
-    
+        
     func slug() -> String {
         let allowed = CharacterSet(charactersIn: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-")
         
