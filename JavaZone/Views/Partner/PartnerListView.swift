590f8bf6d81372565d46f3edc0b4b0d214572602
Chris Searle <chris@chrissearle.org>
2021-09-25 11:55:19 +0200

    More MVVM for partner list

--- JavaZone/Views/Partner/PartnerListView.swift
+++ JavaZone/Views/Partner/PartnerListView.swift
@@ -1,36 +1,8 @@
 import SwiftUI
 import WaterfallGrid
-import os
-import simd
 
 struct PartnerListView: View {
     @StateObject private var viewModel = PartnerViewModel()
-
-    @State private var refreshFatal = false
-
-    @State private var alertItem : AlertItem?
-    
-    @State private var showingScanSheet = false
-
-    private var idiom : UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
-    private var isPortrait : Bool { UIDevice.current.orientation.isPortrait }
-    
-    // TODO - can we get info on screen size here? Calculate out from that?
-    private var cols : Int {
-        if idiom == .pad {
-            if (isPortrait == true) {
-                return 4
-            } else {
-                return 7
-            }
-        } else {
-            if (isPortrait == true) {
-                return 3
-            } else {
-                return 4
-            }
-        }
-    }
     
     var body: some View {
         VStack {
@@ -38,13 +10,13 @@ struct PartnerListView: View {
                 WaterfallGrid(viewModel.displayPartners, id: \.self) { partner in
                     PartnerLogoView(partner: partner)
                 }
-                .gridStyle(columns: cols, spacing: 10)
+                .gridStyle(columns: viewModel.cols, spacing: 10)
                 .padding()
                 .onAppear(perform: {
-                    self.refreshPartners(force: false)
+                    self.viewModel.refreshPartners()
                 })
             }
-            .alert(item: $alertItem) { alertItem in
+            .alert(item: $viewModel.alertItem) { alertItem in
                 Alert(
                     title: alertItem.title,
                     message: alertItem.message,
@@ -60,19 +32,7 @@ struct PartnerListView: View {
         }
     }
 
-    func refreshPartners(force: Bool) {
-        PartnerService.refresh(force: force) { (status, message, logMessage) in
-            
-            // If we fail to fetch but have partners _ this list changes so rarely that we ignore and continue.
-            if (status == .Fail && self.viewModel.partners.count == 0) {
-                self.alertItem = AlertContext.build(title: "Refresh failed", message: message, buttonTitle: "OK")
-            }
-            
-            if (status == .Fatal) {
-                self.alertItem = AlertContext.buildFatal(title: "Refresh failed", message: message, buttonTitle: "OK", fatalMessage: logMessage)
-            }
-        }
-    }
+
 }
 
 struct PartnerListView_Previews: PreviewProvider {
--- JavaZone/Views/Partner/PartnerViewModel.swift
+++ JavaZone/Views/Partner/PartnerViewModel.swift
@@ -1,8 +1,10 @@
+import SwiftUI
 import Combine
 import os.log
 
 class PartnerViewModel : ObservableObject {
     let logger = Logger(subsystem: Logger.subsystem, category: "PartnerViewModel")
+    private let idiom = UIDevice.current.userInterfaceIdiom
 
     @Published var displayPartners : [Partner] = []
     @Published var partners : [Partner] = [] {
@@ -11,6 +13,35 @@ class PartnerViewModel : ObservableObject {
             displayPartners = newValue.shuffled()
         }
     }
+        
+    @Published var alertItem : AlertItem?
+    @Published var cols : Int = 3
+    
+    // TODO - trigger change on orientation change
+    private var isPortrait: Bool = true {
+        willSet {
+            calculateCols()
+        }
+    }
+    
+    // TODO - can we get info on screen size here? Calculate out from that?
+    private func calculateCols() {
+        if idiom == .pad {
+            if (isPortrait == true) {
+                cols = 4
+            } else {
+                cols = 7
+            }
+        } else {
+            if (isPortrait == true) {
+                cols = 3
+            } else {
+                cols = 4
+            }
+        }
+    }
+    
+    
     
     private var cancellable : AnyCancellable?
     
@@ -18,6 +49,20 @@ class PartnerViewModel : ObservableObject {
         cancellable = partnerPublisher.sink { partners in
             self.logger.info("Updating partners")
             self.partners = partners
+        }
+    }
+    
+    func refreshPartners(force: Bool = false) {
+        PartnerService.refresh(force: force) { (status, message, logMessage) in
+            
+            // If we fail to fetch but have partners _ this list changes so rarely that we ignore and continue.
+            if (status == .Fail && self.partners.count == 0) {
+                self.alertItem = AlertContext.build(title: "Refresh failed", message: message, buttonTitle: "OK")
+            }
+            
+            if (status == .Fatal) {
+                self.alertItem = AlertContext.buildFatal(title: "Refresh failed", message: message, buttonTitle: "OK", fatalMessage: logMessage)
+            }
         }
     }
 }
