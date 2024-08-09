a42556b1f5e8dcf183695ac363246f9001d4d33e
Chris Searle <chris@chrissearle.org>
2024-08-09 20:24:29 +0200

    Try to fix release crash on refresh

--- JavaZone/SceneDelegate.swift
+++ JavaZone/SceneDelegate.swift
@@ -17,9 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // (see `application:configurationForConnectingSceneSession` instead).
 
         // Get the managed object context from the shared persistent container.
-        // swiftlint:disable force_cast
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        // swiftlint:enable force_cast
+        let context = CoreDataManager.shared.persistentContainer.viewContext
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
@@ -64,7 +62,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        CoreDataManager.shared.saveContext()
     }
 
 }
